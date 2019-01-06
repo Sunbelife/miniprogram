f92e797b0a7673d8d2c298dae278e21b5e075ef2
杨恩永-home <893579167@qq.com>
2019-01-06 22:44:48 +0800

    mmm

--- miniPrograme/utils/tplConfig.js
+++ miniPrograme/utils/tplConfig.js
@@ -17,8 +17,8 @@ const mp3AudioUrl = [
     'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
 ];
 const mp3Rel = [{
-    music_id: 13,
-    music_name: "XXXX",
+    music_id: -1,
+    music_name: "默认",
     music_type: "1",
     music_upload_time: "2018-12-22",
     music_url: "https://xcx.lyy99.com/uploads/music/default.mp3",
